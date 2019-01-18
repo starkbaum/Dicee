1c121e63198469f05e85fadfcd22510cf5ba19a8
Stefan Starkbaum <starkbaum.stefan@gmail.com>
2019-01-18 13:27:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/dicee/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.android.dicee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button rollButton = (Button) findViewById(R.id.rollButton);
+        final ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+        final ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+        final int[] diceArray = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6,
+        };
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                leftDice.setImageResource(diceArray[getRandomNumber()]);
+                rightDice.setImageResource(diceArray[getRandomNumber()]);
+            }
+        });
+    }
+
+    private int getRandomNumber() {
+        Random randomNumberGenerator = new Random();
+        return randomNumberGenerator.nextInt(6);
+    }
+}
